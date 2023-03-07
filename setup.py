2831f56280b82aa2cb78baa481c035cfd5f5aee2
Alex Zorin <alex@zorin.au>
2023-03-07 18:45:54 +1100

    snap: try something else

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+install_requires = [
+    "scapy",
+    "pyroute2",
+    "fnfqueue"
+]
+if not os.environ.get("SNAP_BUILD"):
+    install_requires.extend(["certbot>=1.25.0", "acme>=1.25.0"])
+else:
+    install_requires.append("packaging")
+
+setup(
+    install_requires=install_requires,
+)
